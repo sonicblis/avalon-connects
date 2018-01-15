d01b5e0a47d5165419c8897ee03f637d96c62f6c
Chris <slashdrew@gmail.com>
2018-01-15 18:56:38 -0500

    fix to control page to show number of attendees

--- src/app/components/control/control.js
+++ src/app/components/control/control.js
@@ -1,22 +1,28 @@
 (function (angular) {
-  function controlController() {
+  function controlController($root) {
     this.clearAttendees = () => {
+      this.attendingSnapshot.forEach((account) => {
+        account.ref.update({ attending: null });
+      });
+    };
+
+    this.$onInit = function () {
       firebase.database()
         .ref('accounts')
         .orderByChild('attending')
         .startAt('')
         .once('value', (snapshot) => {
-          snapshot.forEach((account) => {
-            account.ref.update({ attending: null });
-          });
-      });
+          this.attendingSnapshot = snapshot;
+          this.numberAttending = snapshot.numChildren();
+          $root.$digest();
+        });
     };
   }
 
   angular.module('AvalonConnects')
     .component('control', {
       templateUrl: 'views/control.html',
-      controller: [controlController],
+      controller: ['$rootScope', controlController],
     });
 }(angular));
 
